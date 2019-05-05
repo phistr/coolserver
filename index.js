8ca54412e39aaf14d5ad0c662da7d8d19dc1a6d8
miguel <miguelmu@kth.se>
2019-05-05 16:57:01 +0200

    question mark

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 app.get('/', (req, res) => {
-  res.send('hello world!!!!!')
+  res.send('hello world?')
 })
 
 app.listen(port)
