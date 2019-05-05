df08e2e3bbbb4db21fb6064151c1dc69e83673b7
phistr <phistr@kth.se>
2019-05-05 21:39:08 +0200

    First attempt at coolserver being cool

--- index.js
+++ index.js
@@ -4,8 +4,41 @@ const app = express()
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 app.get('/', (req, res) => {
-  res.send('hello world?')
-})
+	var html = `
+		
+<!DOCTYPE html>
+<html>
+<head>
+<title>Min sida!</title>
+</head>
+<script>
+
+
+function calculate() {
+	var first = document.getElementById(\'first\').value
+	var second = document.getElementById(\'second\').value
+	var answer = document.getElementById(\'answer\')
+
+	answer.innerHTML = +first + +second
+}
+
+
+</script>
+<body>
+
+
+	<input type="number" id="first"> + 
+	<input type="number" id="second"> = 
+	<label id="answer"></label><br>
+	
+	
+	<button onClick="calculate">Calculate</button>
+
+</body>
+</html>
+
+	`;
+  res.send(html)})
 
 app.listen(port)
 console.log('Listening on port ' + port)
